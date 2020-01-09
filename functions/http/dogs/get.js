509595a2b42df5d585a42a3687334623aa21a283
LoicBrassart <loic.brassart@gmail.com>
2020-01-09 17:58:43 +0100

    Added the GET route

--- functions/http/dogs/get.js
+++ functions/http/dogs/get.js
@@ -2,14 +2,15 @@ import * as dynamoDbLib from "../../../libs/dynamodb-lib";
 import { success, failure } from "../../../libs/response-lib";
 
 export async function main(event, context) {
-  const data = event.mock ? event.body : JSON.parse(event.body);
+  const data = event.mock ? event.params : JSON.parse(event.params);
   const params = {
-    TableName: process.env.dogsTableName
+    TableName: process.env.dogsTableName,
+    Key: { id: data.id }
   };
 
   try {
     const res = await dynamoDbLib.call("get", params);
-    return success(res.Item);
+    return success(res);
   } catch (e) {
     return failure({ error: e });
   }
